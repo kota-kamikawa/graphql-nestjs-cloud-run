75212b15b2fa50b8a0ba125be668ca311908cb77
kota-kamikawa <kota.kamikawa@revcomm.co.jp>
2024-03-26 23:12:38 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableShutdownHooks()
-  await app.listen(3000, () => {
+  const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
+  await app.listen(8080, () => {
     console.log(`
-🚀 Server ready at: http://localhost:3000/graphql
+🚀 Server ready at: http://localhost:8080/graphql
 ⭐️ See sample queries: http://pris.ly/e/ts/graphql-nestjs#using-the-graphql-api
-`)
-  })
+`);
+  });
 }
-bootstrap()
+bootstrap();
